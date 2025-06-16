d28dbd6009a9d10c9a3a2274d967d66a49a820a1
rohan2245 <rohan2245a@gmail.com>
2025-06-16 18:08:49 +0530

    Update main.jsx

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -2,7 +2,7 @@
 
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import RenderApp from './App.jsx'; // Make sure this path is correct for your App.jsx
+import App from './App.jsx'; // <--- CHANGE IS HERE: Import App directly as its default export
 import './style.css'; // Import your global stylesheet here
 
 // Get the root DOM element where your React app will be rendered
@@ -11,6 +11,6 @@ const rootElement = document.getElementById('root');
 // Create a React root and render your main application component
 ReactDOM.createRoot(rootElement).render(
   <React.StrictMode>
-    <RenderApp />
+    <App /> {/* <--- CHANGE IS HERE: Render App directly */}
   </React.StrictMode>,
 );
