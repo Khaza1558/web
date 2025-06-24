3e8dbc296505188d5e84b12f2d7c12ba453a7283
Khaza1558 <khaza24680a@gmail.com>
2025-06-24 17:22:13 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -15,7 +15,7 @@ app.use(cors({
 }));
 app.use(express.json());
 
-// app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
+app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
 
 // --- START DEBUG LOG ---
 console.log('Attempting to connect to database...');
@@ -45,4 +45,3 @@ const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
 });
-
